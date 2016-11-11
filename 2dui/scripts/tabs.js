7976f0ddf03cac331c71ecef2ff51c81f987324f
Dizzket <dizzket@users.noreply.github.com>
2016-11-11 03:25:05 -0800

    Changed tabs.js

--- 2dui/scripts/tabs.js
+++ 2dui/scripts/tabs.js
@@ -26,7 +26,7 @@
   
   var exceptme = document.getElementById(containertoload).style.display = "block"; //except for the currently active container
   var exceptme = document.getElementById(tabbuttonthatwaspushed).style.backgroundColor = "#E4E4E4"; //set the background colour for the selected tab
-  var exceptme = document.getElementById(tabbuttonthatwaspushed).style.color = "#545454"; //set the colour for the selected tab
+  var exceptme = document.getElementById(tabbuttonthatwaspushed).style.color = "#242A2D"; //set the colour for the selected tab
 	document.getElementById("windowarea").scrollTop = 0; //scroll back to top of window area div
 
   }
