7f39757113c9bd6950f4e8bc448b44b17eea3ca2
Dizzket <dizzket@users.noreply.github.com>
2016-11-21 13:33:47 -0800

    Fixed script crash relating to server returning NULL

--- 2dui/scripts/partylistgen.js
+++ 2dui/scripts/partylistgen.js
@@ -35,19 +35,26 @@
 	}
 	
 	function extractDomain(url) {
-    var domain;
-    //find & remove protocol (http, ftp, etc.) and get domain
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    }
-    else {
-        domain = url.split('/')[0];
-    }
+		
+		if (url != null)
+		{
+		var domain;
+		//find & remove protocol (http, ftp, etc.) and get domain
+		if (url.indexOf("://") > -1) {
+			domain = url.split('/')[2];
+		}
+		else {
+			domain = url.split('/')[0];
+		}
 
-    //find & remove port number
-    domain = domain.split(':')[0];
-
-    return domain;
+		//find & remove port number
+		domain = domain.split(':')[0];
+		}
+		else
+		{
+		domain = "URL Not Found";	
+		}
+		return domain;
 }
   
     function launchPortal(url) {
