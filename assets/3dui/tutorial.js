d8249bb185293492310bcee3ec55d96382dd6efd
JamesMcCrae <mccrae@dgp.toronto.edu>
2017-03-31 12:25:11 -0700

    add tutorial script

---
+++ assets/3dui/tutorial.js
@@ -0,0 +1,47 @@
+var showinstruct = false;
+
+room.update = function(delta_time) 
+{
+	left_update(player.hmd_type);
+	right_update(player.hmd_type);
+}
+
+room.onMouseDown = function()
+{
+	showinstruct = true;
+}
+
+room.onMouseUp = function()
+{
+	showinstruct = false;
+}
+
+function left_update(hmd_type) 
+{
+	var n = hmd_type + "0";
+  	if (showinstruct && player.hand0_active) {
+      		room.objects[n].visible = true;
+      		room.objects[n].pos  = player.hand0_pos;
+      		room.objects[n].xdir = player.hand0_xdir;
+      		room.objects[n].ydir = player.hand0_ydir;
+      		room.objects[n].zdir = player.hand0_zdir;
+  	} 
+  	else {
+      		room.objects[n].visible = false;
+  	}
+}
+
+function right_update(hmd_type) 
+{
+	var n = hmd_type + "1";
+  	if (showinstruct && player.hand1_active) {
+    		room.objects[n].visible = true;
+    		room.objects[n].pos  = player.hand1_pos;
+    		room.objects[n].xdir = player.hand1_xdir;
+    		room.objects[n].ydir = player.hand1_ydir;
+    		room.objects[n].zdir = player.hand1_zdir;
+  	} 
+  	else {
+    		room.objects[n].visible = false;
+  	}
+}
