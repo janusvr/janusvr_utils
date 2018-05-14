81bb1be0016ec63967b3fed802c5289e66ece257
Spyduck <flock.of.murkrows@gmail.com>
2018-05-14 15:19:04 -0400

    use vesta trending api instead of current popular

--- assets/3dui/dashboard/scripts/main.js
+++ assets/3dui/dashboard/scripts/main.js
@@ -4,12 +4,13 @@ var interval;
 var intervalParty;
 var intervalPopular;
 var intervalPopulateProperties;
+var trendingdata = [];
 
 window.onload = function() {
 
 interval = setInterval(function(){ populateBookmarks() }, 250);
 intervalPopulateProperties = setInterval(function(){ populateProperties() }, 100);
-
+updatetrendingdata();
 }
 
 function populateBookmarks() {
@@ -28,7 +29,6 @@ function populateParty(force) {
 
   if ( ( (typeof window.janus.partymodedata != "undefined") && (window.janus.partymodedata.length > 0) ) || (force == 1) )
     {
-
       refreshPartymode();
 
       window.janus.updatepartymodedata();
@@ -44,7 +44,7 @@ function populatePopular() {
 
   if (typeof window.janus.populardata != "undefined" && window.janus.populardata.length > 0)
     {
-      refreshPopular()
+      refreshPopular();
       clearInterval(intervalPopular);
 
     }
@@ -59,7 +59,7 @@ function populateProperties() {
       window.janus.updatepartymodedata();
       window.janus.updatepopulardata("?orderBy=weight&desc=true&limit=50");
       intervalParty = setInterval(function(){ populateParty() }, 250);
-      intervalPopular = setInterval(function(){ populatePopular() }, 250);
+      intervalPopular = setInterval(function(){ refreshTrending() }, 250);
       clearInterval(intervalPopulateProperties);
 
     }
@@ -162,7 +162,7 @@ function refreshPartymode() {
 
 
 function refreshPopular() {
-
+	
     //Populate list with now available window.janus.bookmarks array.
     var blacklist_urls = ['https://vesta.janusvr.com/popular/all/1', 'https://vesta.janusvr.com/recent/all/1', 'https://www.reddit.com/r/VRsites/', 'https://www.youtube.com', 'https://www.janusvr.com/newlobby/index.html', 'https://vesta.janusvr.com/sandbox', 'https://janusvr.com/help', 'https://vesta.janusvr.com/chat', 'https://janusvr.com/help/' ];
     emptyElement(document.getElementById("popularList"));
@@ -184,4 +184,49 @@ function refreshPopular() {
 
     }
 
+}
+function refreshTrending() {
+
+    //Populate list with now available window.janus.bookmarks array.
+    var blacklist_urls = ['https://vesta.janusvr.com/popular/all/1', 'https://vesta.janusvr.com/recent/all/1', 'https://www.reddit.com/r/VRsites/', 'https://www.youtube.com', 'https://www.janusvr.com/newlobby/index.html', 'https://vesta.janusvr.com/sandbox', 'https://janusvr.com/help', 'https://vesta.janusvr.com/chat', 'https://janusvr.com/help/' ];
+    emptyElement(document.getElementById("popularList"));
+
+    for (var i=0;i<trendingdata.length;i++)
+    {
+
+      var thumb= trendingdata[i].thumbnail;
+
+      var title= trendingdata[i].roomName || extractDomain(trendingdata[i].roomUrl) || "Untitled Webspace";
+
+      var url = trendingdata[i].roomUrl;
+
+
+      if (blacklist_urls.indexOf(url) === -1)
+	  {
+        document.getElementById("popularList").innerHTML += "<div class='tile popular' onclick='window.janus.launchurl(\""+url+"\",0)' style='background: url(\""+thumb+"\") no-repeat center/cover;' ><div class='infoHolder'>"+strip(title)+"</div></div>";
+      }
+
+    }
+
+}
+
+updatetrendingdata = function(params)
+{
+	fetch('https://vesta.janusvr.com/api/top_rooms?limit=36&orderBy=trending', { method:'get' })
+	.then(function(response)
+	{
+		if (!response.ok)
+		{
+			throw Error(response.statusText);
+		}
+		return response.json();
+	})
+	.then(function(data)
+	{
+		trendingdata = data.data;
+	})
+	.catch(function() {
+		console.log("error loading activity");
+	});
+	trendingTimeout = setTimeout(function(){ updatetrendingdata() }, 30000);
 }
